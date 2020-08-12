565acf7f80847508930c981960ea45bfc4edf194
Ilya Solovyov <mail@erdenian.ru>
2020-08-12 22:40:11 +0300

    Setup Gradle plugin configuration and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,21 +2,32 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.3.72"
+    `java-gradle-plugin`
 }
 
 group = "ru.erdenian"
 version = "0.1"
 
 repositories {
+    google()
     jcenter()
 }
 
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
 tasks.withType<KotlinCompile>().all {
-    kotlinOptions.freeCompilerArgs += "-Xopt-in=kotlin.RequiresOptIn"
+    kotlinOptions {
+        jvmTarget = "1.8"
+        @Suppress("SuspiciousCollectionReassignment")
+        freeCompilerArgs += "-Xopt-in=kotlin.RequiresOptIn"
+    }
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.8")
+    implementation("com.android.tools.build:gradle:4.0.1")
     implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.7.1")
 }
