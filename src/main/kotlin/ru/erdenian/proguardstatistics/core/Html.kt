732467c4d018e0c7472fb3eb47bda0212d3db9ed
Ilya Solovyov <mail@erdenian.ru>
2020-08-12 23:09:40 +0300

    Html generation refactoring

--- src/main/kotlin/ru/erdenian/proguardstatistics/core/Html.kt
+++ src/main/kotlin/ru/erdenian/proguardstatistics/core/Html.kt
@@ -9,14 +9,62 @@ import kotlinx.html.html
 import kotlinx.html.id
 import kotlinx.html.label
 import kotlinx.html.li
-import kotlinx.html.link
 import kotlinx.html.stream.appendHTML
+import kotlinx.html.style
+import kotlinx.html.title
 import kotlinx.html.ul
+import kotlinx.html.unsafe
 import java.io.Writer
 import java.lang.Long.signum
 import java.text.CharacterIterator
 import java.text.StringCharacterIterator
+import java.time.ZonedDateTime
 import kotlin.math.abs
+
+fun Writer.appendStructureHtml(root: PackageNode) = appendHTML(prettyPrint = false).html {
+    head {
+        title("ProGuardStatistics report")
+        style { unsafe { +readResource("/styles.css") } }
+    }
+    body {
+        div("css-treeview") {
+            ul {
+                fun createRecursive(packageNode: PackageNode) {
+                    li {
+                        checkBoxInput { id = packageNode.name }
+                        label {
+                            htmlFor = packageNode.name
+                            text(packageNode.stringify())
+                        }
+
+                        ul {
+                            packageNode.subpackages.forEach { createRecursive(it) }
+
+                            packageNode.classes.forEach { classNode ->
+                                li {
+                                    checkBoxInput { id = classNode.packageName + classNode.name }
+                                    label {
+                                        htmlFor = classNode.packageName + classNode.name
+                                        text(classNode.stringify())
+                                    }
+
+                                    ul {
+                                        classNode.fields.forEach { li { text(it.stringify()) } }
+                                        classNode.methods.forEach { li { text(it.stringify()) } }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                createRecursive(root)
+
+                hr()
+                +"Generated at ${ZonedDateTime.now()}"
+            }
+        }
+    }
+}
 
 private fun humanReadableByteCountBin(bytes: Long): String? {
     val absB = if (bytes == Long.MIN_VALUE) Long.MAX_VALUE else abs(bytes)
@@ -42,46 +90,4 @@ private fun BaseNode.stringify() = String.format(
     humanReadableByteCountBin(shrankSize.toLong())
 )
 
-fun Writer.appendStructureHtml(root: PackageNode) = appendHTML(prettyPrint = false).html {
-    head { link("result.css", "stylesheet", "text/css") }
-    body {
-        div("css-treeview") {
-            ul {
-                fun createRecursive(packageNode: PackageNode) {
-                    li {
-                        checkBoxInput { id = packageNode.name }
-                        label {
-                            htmlFor = packageNode.name
-                            text(packageNode.stringify())
-                        }
-
-                        ul {
-                            packageNode.subpackages.forEach { createRecursive(it) }
-
-                            packageNode.classes.forEach { classNode ->
-                                li {
-                                    checkBoxInput { id = classNode.packageName + classNode.name }
-                                    label {
-                                        htmlFor = classNode.packageName + classNode.name
-                                        text(classNode.stringify())
-                                    }
-
-                                    ul {
-                                        classNode.fields.forEach { fieldNode ->
-                                            li { text(fieldNode.stringify()) }
-                                        }
-                                        classNode.methods.forEach { methodNode ->
-                                            li { text(methodNode.stringify()) }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-                createRecursive(root)
-                hr()
-            }
-        }
-    }
-}
+private fun readResource(path: String) = {}::class.java.getResource(path).readText()
