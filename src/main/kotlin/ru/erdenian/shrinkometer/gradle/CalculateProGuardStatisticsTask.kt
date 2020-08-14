fec4e7eedc6c1f88a266de1d50a6a7e8b3a1f9d5
Ilya Solovyov <mail@erdenian.ru>
2020-08-14 14:37:25 +0300

    Remove unused parameter from taskAction

--- src/main/kotlin/ru/erdenian/shrinkometer/gradle/CalculateProGuardStatisticsTask.kt
+++ src/main/kotlin/ru/erdenian/shrinkometer/gradle/CalculateProGuardStatisticsTask.kt
@@ -6,7 +6,6 @@ import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
-import org.gradle.work.InputChanges
 import ru.erdenian.shrinkometer.core.appendStructureHtml
 import ru.erdenian.shrinkometer.core.readAndCompare
 import java.io.File
@@ -32,7 +31,7 @@ open class CalculateProGuardStatisticsTask : DefaultTask() {
     lateinit var reportFile: File
 
     @TaskAction
-    internal fun taskAction(inputs: InputChanges) {
+    internal fun taskAction() {
         fun execAnalyzer(apk: File, params: String) =
             Runtime.getRuntime().exec("${apkAnalyzerFile.get()} dex packages $apk $params").inputStream.reader()
 
